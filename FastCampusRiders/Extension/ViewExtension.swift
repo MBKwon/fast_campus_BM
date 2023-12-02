d49b357ad6b725a9faa3cbfdacba8e0572ae5d93
Brad MB <maskkwon@gmail.com>
2023-12-02 12:21:24 +0900

    [no] add the HitTest Example

--- FastCampusRiders/Extension/ViewExtension.swift
+++ FastCampusRiders/Extension/ViewExtension.swift
@@ -14,3 +14,25 @@ extension UIView {
         return self
     }
 }
+
+extension UIView {
+    class func swizzleMethod() {
+        let originalSelector = #selector(hitTest)
+        let swizzleSelector = #selector(myHitTest)
+
+        guard let originMethod = class_getInstanceMethod(UIView.self, originalSelector),
+              let swizzleMethod = class_getInstanceMethod(UIView.self, swizzleSelector)
+        else { return }
+
+        method_exchangeImplementations(originMethod, swizzleMethod)
+    }
+
+    @objc public func myHitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let hitView = self.myHitTest(point, with: event)
+        if let myView = hitView, myView == self {
+            print("Touch : \(myView)")
+        }
+
+        return hitView
+    }
+}
